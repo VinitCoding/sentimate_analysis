67bb0c1a19e7152191794402a02654ab4f44eac9
atharvganla-chistats <atharv.ganla@chistats.com>
2024-06-22 14:29:25 +0530

    commit

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -4,7 +4,7 @@ import chistats_logo from '../assets/chistat_logo.png'
 const Navbar = () => {
   return (
     <div className='flex justify-between'>
-      <h2 className='text-xl font-semibold text-darkBlue'>Sentimate Analysis</h2>
+      <h2 className='text-xl font-semibold text-darkBlue'>Sentiment Analysis</h2>
       <img src={chistats_logo} alt="logo" className='w-28 h-6'/>
     </div>
   )
